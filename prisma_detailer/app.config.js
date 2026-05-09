bdbb794bf420ca5663ff1eaccba68ffda4aaf8cc
Giftezra <Giftezraifeanyi@gmail.com>
2026-05-09 18:17:46 +0100

    Final push with debug

--- prisma_detailer/app.config.js
+++ prisma_detailer/app.config.js
@@ -7,12 +7,12 @@ const appEnv = process.env.EXPO_PUBLIC_APP_ENV || "development";
 
 const envUrls = {
   production: {
-    detailer_app_url: "https://detailer.prismavalet.com",
+    detailer_app_url: "https://crew.prismavalet.com",
     customer_app_url: "https://client.prismavalet.com",
     websockets_url: "wss://client.prismavalet.com/ws/detailer/",
   },
   staging: {
-    detailer_app_url: "https://staging.detailer.prismavalet.com",
+    detailer_app_url: "https://staging.crew.prismavalet.com",
     customer_app_url: "https://staging.client.prismavalet.com",
     websockets_url: "wss://staging.client.prismavalet.com/ws/detailer/",
   },
